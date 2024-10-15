8dd522518ed34a3bc2dc1e4cd0edf642c6a7d4f4
aclla12 <2301163@s.asojuku.ac.jp>
2024-10-15 10:15:23 +0900

    3-1を作成

--- 3-2/script/script.js
+++ 3-2/script/script.js
@@ -1,1 +1,18 @@
 // JavaScript用ファイル
+// JavaScript用ファイル
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods: {
+       increment() {
+            this.coutn++;
+       },
+       decrement() {
+            this.count--;
+       }
+    }
+});
